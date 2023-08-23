a1d06e2f07b5daabb862864bb3794b81eb650bc8
Momin <knightrideraiz@gmail.com>
2023-08-24 01:08:52 +0530

    chsnges

--- serverFunctions.js
+++ serverFunctions.js
@@ -1,5 +1,5 @@
 import { renderFile } from "ejs"
-import PostBlog from './models/postBlog';
+import PostBlog from './models/postBlog.js';
 
 const homeStartingContent = "Welcome to Narrative Nest, a haven for words and stories..."; // Your content here
 const aboutContent = "At Narrative Nest, we believe that every person is a storyteller..."; // Your content here
